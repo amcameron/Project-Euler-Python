6678f5771048d1337a3bd03bac90941563d49808
Andrew Cameron <amcameron@gmail.com>
2009-08-15 16:20:10 -0700

    improving ispalindrome() to return booleans

--- Problem4.py
+++ Problem4.py
@@ -1,9 +1,8 @@
 def ispalindrome(n):
-	if str(n)==str(n)[::-1] : return 1
-	else: return 0
+	return str(n)==str(n)[::-1]
 
 curbest = 0
 for i in range(1000,0,-1):
 	for j in range(1000,i,-1):
-		if (ispalindrome(i*j) == 1 and i*j > curbest): curbest = i*j
+		if (ispalindrome(i*j) and i*j > curbest): curbest = i*j
 print curbest	
