1692aa8cf20f29fe896e3b27a7a5f85ba47d42d2
Andrew Cameron <amcameron@gmail.com>
2009-08-16 13:41:51 -0700

    added the math module so sqrt() works

--- Problem7.py
+++ Problem7.py
@@ -1,3 +1,4 @@
+import math
 def nextprime(n):
 	if n%2 == 0: i = n + 1
 	else: i = n + 2
@@ -8,7 +9,7 @@ def nextprime(n):
 
 def isprime(n):
 	i = 2
-	while (i <= sqrt(n)):
+	while (i <= math.sqrt(n)):
 		if (n % i == 0): return 0
 		else: i = i + 1
 	return 1
