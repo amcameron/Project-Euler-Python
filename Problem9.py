48f00741e088b9b3dbb15314084ec030d966b864
Andrew Cameron <amcameron@gmail.com>
2009-08-20 09:23:53 -0700

    improving solution to Problem 9 to use smart bounds, implicit value for c, double for loops, and some interesting Python syntax.

--- Problem9.py
+++ Problem9.py
@@ -1,16 +1,1 @@
-import math
-a = b = c = 1
-while True:
-	a += 1
-	b = a
-	while a + b + c <= 1000:
-		b += 1
-		c = int(math.sqrt(a*a + b*b))
-		if c*c != a*a + b*b: continue
-		else:	#i.e. if a*a + b*b == c*c
-			if a+b+c == 1000: break
-	if c*c != a*a + b*b: continue
-	else:
-		if a+b+c == 1000: break
-
-print 'a=',a,' b=',b,' c=',c,' a+b+c=',a+b+c,' abc=',a*b*c
+print [(a,b,(1000-a-b)) for b in xrange(1,500) for a in xrange(1,b) if a**2 + b**2 == (1000-a-b)**2]
