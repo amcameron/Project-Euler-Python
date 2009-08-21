0b40068dcb68972dbe46c42c31cdd2a083286602
Andrew Cameron <amcameron@gmail.com>
2009-08-21 11:56:30 -0700

    made the loops a little more natural and sensical. next: implement sieve-based primality test.

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 class IsPrime():
 	primes = []
 	def __init__(self):
-		self.primes = [2]
+		self.primes = [2,3]
 	def __call__(self, n):
-		i = 0
-		while (self.primes[i]**2 <= n):
-			if (n % self.primes[i] == 0): return False
-			else:
-				i += 1
-				if len(self.primes) == i:
-					self.primes[i:i] = [j for j in xrange(self.primes[-1]+1,self.primes[-1]**2) if [j%prime for prime in self.primes].count(0)==0]
-		return True
+		if self.primes.count(n) != 0: return True
+		while self.primes[-1]**2 < n:
+			i = len(self.primes)
+			self.primes[i:i] = [j for j in xrange(self.primes[-1]+2,self.primes[-1]**2,2) if [j%prime for prime in self.primes].count(0)==0]
+		
+		if [n%prime for prime in self.primes].count(0) != 0:
+			return False
+		else: return True
