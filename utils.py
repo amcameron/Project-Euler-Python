63fb1ef6c607e933570480f4ab64550250743da7
Andrew Cameron <amcameron@gmail.com>
2009-08-23 19:59:19 -0700

    improved IsPrime()
    IsPrimes now handles cases where the input is greater
    than the square of the largest known prime, but has a
    factor that is known.

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 class IsPrime():
 	"""Determine whether a given number is prime or compound.
 
@@ -12,13 +14,32 @@ class IsPrime():
 	def __call__(self, n):
 		"""Return true if n is prime; false otherwise."""
 
-		# if n is already in the list of known primes, it is prime.
+		# check if the input is a known prime
 		if self.primes.count(n) != 0: return True
 
-		# self.primes may yet be too short to state that n is compound;
+		# maybe one of the known primes is a factor
+		initnumprimes = len(self.primes)
+		for prime in self.primes: 
+			if n%prime == 0: return False
+			if prime**2 > n: return True
+
+		# self.primes is too short to state yet that n is compound;
 		# in this case, extend self.primes.  self.primes has to extend to
 		# at least the square root of n.
-		while self.primes[-1] ** 2 < n:
+		self.extend(sqrt(n))
+
+		# now we know that we have a large enough prime to check if n is composite
+		# but we've already checked some, so check the rest as necessary.
+		for prime in self.primes[initnumprimes:]:
+			if n%prime == 0: return False
+			if prime**2 > n: return True
+
+	def extend(self, n):
+		"""Extend the list of known primes to at least n."""
+
+		# Depending on what the last currently-known prime is, we may get bonus
+		# primes AT NO EXTRA COST!!1!one
+		while self.primes[-1] < n:
 			# index all integers up until and including the largest we
 			# can currently check definitively.
 			newprimes = range(0, self.primes[-1] ** 2)
@@ -30,20 +51,4 @@ class IsPrime():
 			# and, lastly, append the remaining (prime) numbers to self.primes
 			l = len(self.primes)
 			self.primes.extend([i for i in newprimes[self.primes[-1]+1:] if i != 0])
-
-		# this if statement is pretty much implied by the previous while loop.
-		# anyway; we now have enough primes to know for sure if n is compound or not.
-		if self.primes[-1] ** 2 >= n:
-			for prime in self.primes:
-				if n%prime == 0: return False
-				if prime ** 2 > n: break
-			return True
-
-	def extend(self, n):
-		"""Extend the list of known primes to at least n."""
-
-		# If n is prime, calling self with n**2 will ensure n is in self.primes.
-		# Depending on what the last currently-known prime is, we may get bonus
-		# primes AT NO EXTRA COST!!1!one
-		self(n**2)
 
