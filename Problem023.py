7bdc2c52a321e11ca1371ce8eb931435da2ec736
Andrew Cameron <amcameron@gmail.com>
2009-12-14 11:38:37 -0500

    add framework for Problem 23

---
+++ Problem023.py
@@ -0,0 +1,74 @@
+def primeFactors(n):
+	"""Find the prime factors of an integer n.
+
+	For example, the prime factors of 12 are
+	2, 2, and 3.
+	"""
+
+	raise NotImplementedError("primeFactors is not yet implemented.")
+
+
+def properFactors(n):
+	"""Find the proper factors of an integer n.
+
+	For example, the proper factors of 12 are
+	1, 2, 3, 4, and 6.
+	"""
+
+	# The proper factors of a number can be found by taking
+	# the product of all unique combinations of its prime factors,
+	# except for all of the prime factors (this just yields the number).
+	# For example, the prime factorization of 12 yiels [2, 2, 3].
+	# The possible combinations are:
+	# [2, 2, 3] nCr 0: []
+	# [2, 2, 3] nCr 1: [2, 3]
+	# [2, 2, 3] nCr 2: [[2, 2], [2, 3]]
+	# [2, 2, 3] nCr 3: not included, because prod([2, 2, 3]) = 12.
+	# The products of these are [1], [2, 3], and [4, 6], respectively.
+	# Therefore, the proper factors of 12 are [1, 2, 3, 4, 6].
+	raise NotImplementedError("properFactors is not yet implemented.")
+
+
+def isAbundant(n):
+	"""A number n is abundant if the sum of its proper factors
+	is greater than the number itself.
+
+	For example, 12 is abundant because 1 + 2 + 3 + 4 + 6 = 16,
+	and 16 > 12.
+	"""
+
+	return properFactors(n) > n
+
+
+def isPerfect(n):
+	"""A number n is perfect if the sum of its proper factors
+	is exactly equal to the number itself.
+
+	For example, 6 is perfect because 1 + 2 + 3 = 6.
+	"""
+
+	return properFactors(n) == n
+
+
+def isDeficient(n):
+	"""A number n is deficient if the sum of its proper factors
+	is less than the number itself.
+
+	For example, 15 is deficient because 1 + 3 + 5 = 8,
+	and 8 < 15.
+	"""
+
+	return properFactors(n) < n
+
+
+def abundantSum(n):
+	"""Return True if a number n can be expressed as the sum of two
+	abundant numbers, False otherwise.
+
+	"""
+
+	raise NotImplementedError("abundantSum is not yet implemented.")
+
+
+if __name__ == "__main__":
+	print sum([i for i in xrange(28124) if not abundantSum(i)])
