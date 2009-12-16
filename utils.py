5533e245a0b8a4af898db8f15627fa095e9eb24e
Andrew Cameron <amcameron@gmail.com>
2009-12-15 20:36:26 -0500

    comment utils and explain how IsPrime works!

--- utils.py
+++ utils.py
@@ -4,12 +4,25 @@ class IsPrime():
 		self.primes = [2,3]
 	def __call__(self, n):
 		if self.primes.count(n) != 0: return True
+
+		# self.primes may yet be too short to state that n is compound;
+		# in this case, extend self.primes.  self.primes has to extend to
+		# at least the square root of n.
 		while self.primes[-1]**2 < n:
+			# index all integers up until and including the largest we
+			# can currently check definitively.
 			newprimes = range(0,self.primes[-1]**2)
+
+			# turn off all numbers that are multiples of known primes.
 			for prime in self.primes:
 				newprimes[prime::prime] = [0 for i in newprimes[prime::prime]]
+
+			# and, lastly, append the remaining (prime) numbers to self.primes
 			l = len(self.primes)
 			self.primes[l:l] = [i for i in newprimes[self.primes[-1]+1:] if i != 0]
+
+		# this if statement is pretty much implied by the previous while loop.
+		# anyway; we now have enough primes to know for sure if n is compound or not.
 		if self.primes[-1]**2 >= n:
 			for prime in self.primes:
 				if n%prime == 0: return False
