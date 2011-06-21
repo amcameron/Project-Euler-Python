6a77f7d7a188bdc74d309e0c0315c396288e73a7
Andrew Cameron <andrew@acameron.org>
2011-06-20 23:02:00 -0400

    fix cheat in Problem 14 and implement better solution

--- Problem014.py
+++ Problem014.py
@@ -5,12 +5,18 @@ def collatz(n):
 		return 3*n+1
 
 curbestlen = curbeststart = 0
-for i in xrange(500001,1000000):
+cache = dict()
+for i in xrange(1, 1000000):
 	n = i
 	l = 1
 	while n != 1:
-		n = collatz(n)
-		l += 1
+		if cache.get(n) is not None:
+			l += cache[n]
+			n = 1
+		else:
+			n = collatz(n)
+			l += 1
+	cache[i] = l
 	if l > curbestlen:
 		curbestlen = l
 		curbeststart = i
