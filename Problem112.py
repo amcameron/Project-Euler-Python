f6d443d8d85cc722ec3ccd34f7f7265ce80c876d
Andrew Cameron <andrew@acameron.org>
2011-06-20 23:54:49 -0400

    add unworking solution to Problem 112

---
+++ Problem112.py
@@ -0,0 +1,35 @@
+"""Find the least number for which the proportion of bouncy numbers is exactly
+99%."""
+
+def isIncreasing(num):
+	last = 0
+	for d in str(num):
+		if int(d) < last:
+			return False
+		else:
+			last = int(d)
+
+	return True
+
+def isDecreasing(num):
+	last = 9
+	for d in str(num):
+		if int(d) > last:
+			return False
+		else:
+			last = int(d)
+
+	return True
+
+def isBouncy(num):
+	return not isIncreasing(num) and not isDecreasing(num)
+
+if __name__ == '__main__':
+	numBouncy = 1
+	i = 1
+	while (numBouncy*100 != i*99):
+		i += 1
+		if (isBouncy(i)):
+			numBouncy += 1
+	
+	print i
